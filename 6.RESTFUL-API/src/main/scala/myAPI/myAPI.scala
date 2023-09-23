6efdaef0f3a9d4028527806a397a48d18cefe823
Wander Gonzalez <wandergonzalez0@hotmail.com>
2023-09-23 11:50:50 -0400

    Modify Api Akka minimal implementetion to work with lowlevel code instead of DSL

--- 6.RESTFUL-API/src/main/scala/myAPI/myAPI.scala
+++ 6.RESTFUL-API/src/main/scala/myAPI/myAPI.scala
@@ -4,21 +4,37 @@ import akka.actor.typed.ActorSystem
 import akka.actor.typed.scaladsl.Behaviors
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
-import akka.http.scaladsl.server.Directives._
+import akka.http.scaladsl.model.HttpMethods._
 import scala.io.StdIn
 
-object myAPI extends App {
-    implicit val system = ActorSystem(Behaviors.empty, "my-system")
-    implicit val executionContext = system.executionContext
+import scala.concurrent.ExecutionContext
 
-    val route =
-    path("hello") {
-        get {
-            complete(HttpEntity(ContentTypes.`text/html(UTF-8)`,"<h1>Say hello to akka-http</h1>"))
-        }
+
+object myAPI extends App {
+    implicit val system = ActorSystem(Behaviors.empty, "my-lowlevel-system")
+    implicit val executionContext: ExecutionContext = system.executionContext
+
+
+    val requestHandler: HttpRequest => HttpResponse = {
+        case HttpRequest(GET, Uri.Path("/"), _, _, _) =>
+            HttpResponse(entity = HttpEntity(
+                ContentTypes.`text/html(UFT-8)`,
+                "<html><body>Hello world!</body></html>"
+            ))
+        
+        case HttpRequest(GET, Uri.Path("/ping"), _, _, _) =>
+        HttpResponse(entity = "PONG!")
+
+        case HttpRequest(GET, Uri.Path("/crash"), _, _, _) =>
+        sys.error("BOOM!")
+
+        case r: HttpRequest =>
+            r.discardEntityBytes() // important to drain incoming HTTP Entity stream
+            HttpResponse(404, entity = "Unknown resource!")
     }
 
-    val bindingFuture = Http().newServerAt("localhost", 8080).bind(route)
+    val bindingFuture = Http().newServerAt("localhost", 8080).bindSync(requestHandler)
+    println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
 
     StdIn.readLine()
 
